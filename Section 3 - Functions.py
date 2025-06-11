01ab8e06fafe8f6d85afbaf22d0c00e725da155f
Chris Prunotto <Chris.Prunotto@gmail.com>
2025-06-10 22:23:49 -0500

    Made it to lesson 8

---
+++ Section 3 - Functions.py
@@ -0,0 +1,21 @@
+#Section 3, lesson 8
+# Exploring python's built in functions
+
+import random
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+
+import random, sys, os, math
+from random import *
+print(randint(1,10))
+
+import sys
+
+
+# You can also install third-party modules using the pip tool. One such example is pyperclip,
+# which will be used in this course.
